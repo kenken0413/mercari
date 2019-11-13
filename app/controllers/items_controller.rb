df39d6a27458c808218d6566e3211d587904ef7b
showaboy <kenta.arai@hotmail.com>
2019-11-13 13:51:28 +0900

    item-sell-view

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,4 +3,13 @@ class ItemsController < ApplicationController
   def index
   end
 
+  def sell
+  end
+
+  def detail
+  end
+
+  def buy
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'items#index'
+  get '/sell', to: 'items#sell'
+  get '/detail', to: 'items#detail'
+  get '/buy', to: 'items#buy'
 
 end
