94d0034e8602430283a3066ed61d7dcf4e959631
showaboy <kenta.arai@hotmail.com>
2019-11-21 17:59:46 +0900

    item_detail_function_1

---
+++ db/fixtures/development/07_image.rb
@@ -0,0 +1,29 @@
+Image.seed do |s|
+  s.id = 1
+  s.item = Item.find(1)
+  s.image = "bag-photo"
+end
+
+Image.seed do |s|
+  s.id = 2
+  s.item = Item.find(2)
+  s.image = "bag-brand-photo"
+end
+
+Image.seed do |s|
+  s.id = 3
+  s.item = Item.find(3)
+  s.image = "sample1"
+end
+
+Image.seed do |s|
+  s.id = 4
+  s.item = Item.find(4)
+  s.image = "mes"
+end
+
+Image.seed do |s|
+  s.id = 5
+  s.item = Item.find(5)
+  s.image = "bag-photo"
+end
---
+++ db/migrate/20191120071018_remove_seller_buyer_from_ites.rb
@@ -0,0 +1,6 @@
+class RemoveSellerBuyerFromItes < ActiveRecord::Migration[5.0]
+  def change
+    remove_column :items, :seller_id, :integer
+    remove_column :items, :buyer_id, :integer
+  end
+end
