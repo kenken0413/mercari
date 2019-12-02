873d93a30ff6f8ffce3c811bc8f106c6d37f3d59
showaboy <kenta.arai@hotmail.com>
2019-12-02 14:17:24 +0900

    user_card_show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,6 +18,12 @@ class UsersController < ApplicationController
   def card_index
     @parents = Category.all.order("ancestry ASC").limit(13)
     @popular_brands = Brand.find(2447,3813,4818,1854)
+    @credit = Credit.where(user_id: current_user.id).first if Credit.where(user_id: current_user.id).present? 
+    if @credit.present?
+      Payjp.api_key = 'sk_test_9581dc90803e604af65e7f4c'
+      customer = Payjp::Customer.retrieve(@credit.customer_id)
+      @credit_information = customer.cards.retrieve(@credit.card_id)
+    end
   end
 
   def card_registrations
