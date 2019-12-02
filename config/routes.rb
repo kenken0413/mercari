0fa08641384ee4e5568c1b8ec82594878cdf2c04
shi-ba-ta-1 <56523540+shi-ba-ta-1@users.noreply.github.com>
2019-12-02 22:00:20 +0900

    credits-index&delete

--- config/routes.rb
+++ config/routes.rb
@@ -19,7 +19,7 @@ Rails.application.routes.draw do
     end
   end
 
-  resources :credits, only: [:new,:create] do
+  resources :credits, only: [:new,:create, :destroy] do
     collection do
       get :redirect_users_card_index
       get :redirect_registrations_end
